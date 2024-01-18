af84c921184e25a5def26e7074b30c4f019c8668
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2024-01-18 19:47:50 +0530

    add giniCommands util

---
+++ src/utils/giniCommands.ts
@@ -0,0 +1,11 @@
+enum GiniCommands {
+  Run = "gini.run",
+  Optimize = "gini.optimize",
+  Transpile = "gini.transpile",
+  Annotate = "gini.annotate",
+  Deconstruct = "gini.deconstruct",
+  Update = "gini.update",
+  Copy = "gini.copy",
+}
+
+export default GiniCommands;
