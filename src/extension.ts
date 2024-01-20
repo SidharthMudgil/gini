911fd35fa2c8b25ebd8ff5a82709bcdac1ac854e
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2024-01-20 19:49:01 +0530

    add logo

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,14 @@
 import * as vscode from 'vscode';
+import getGeminiApiKey from './utils/getConfig';
 
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "gini" is now active!');
 
 	let disposable = vscode.commands.registerCommand('gini.run', () => {
-		vscode.window.showInformationMessage('Hello World from Sidharth!');
-		vscode.window.showErrorMessage("wronf");
+		getGeminiApiKey();
+		// vscode.window.showInformationMessage('Hello World from Sidharth!');
+		// vscode.window.showErrorMessage("wronf");
 	});
 
 	context.subscriptions.push(disposable);
